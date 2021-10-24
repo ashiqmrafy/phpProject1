865d42e5e5474f52aff5f6d89a7f7ba78d0594dc
Ashiq Rafy <ashiqrafy@Ashiqs-MBP.local.tld>
2021-10-24 00:48:33 -0400

    Committing Index and DB connect

---
+++ shop.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<html>
+    <head>
+        <h1>BookStore</h1>
+        <span><a href="index.html">Home</a> | <a href="shop.php">Shop Now</a> | <a></a></span>
+    </head>
+    <body>
+        
+    </body>
+</html>
