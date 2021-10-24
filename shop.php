2aef541a28b0dd6ee4b9d3b71d0df15e06f72f95
Ashiq Rafy <ashiqrafy@Ashiqs-MBP.local.tld>
2021-10-24 02:03:11 -0400

    Committing Index and Shop.php changes

--- shop.php
+++ shop.php
@@ -1,11 +1,15 @@
-<?php
-?>
 <html>
-    <head>
-        <h1>BookStore</h1>
-        <span><a href="index.html">Home</a> | <a href="shop.php">Shop Now</a> | <a></a></span>
-    </head>
-    <body>
-        
-    </body>
+<body>
+<h1>BookStore</h1>
+<span><a href="index.html">Home</a> | <a href="shop.php">Shop Now</a> | <a></a></span>
+    <?php
+    session_start();
+    require("mysqli_connect.php");
+    $q = "SELECT * FROM BookInventry";
+    $r = mysqli_query($dbc, $q);
+    while($row = mysqli_fetch_array($r)){
+        echo "<p>".$row["bookname"]."</p>";
+    }
+    ?>    
+</body>
 </html>
